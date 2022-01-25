9cf9fca5b52567c5e72b561f875a0f3bd804e56e
Tyler Paneitz <Tyler.Paneitz@vu.com>
2022-01-25 16:02:57 -0600

    str casting

--- payCalculator.py
+++ payCalculator.py
@@ -14,14 +14,14 @@ def calculatePay():
         overtime = hrs-40
         overtime *= (rate*1.5)
         pay = (40*rate)+overtime
-        print ("Pay: " + pay)
+        print ("Pay: " + str(pay))
     else:
         pay = hrs*rate
-        print ("Pay: " + pay)
+        print ("Pay: " + str(pay))
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-## calculatePay()
+calculatePay()
